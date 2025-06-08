9901402cff4ed18314c005d7b3d2b0f718fcd707
Clodrax <ehmedlicelal2@gmail.com>
2025-06-08 13:43:03 +0400

    first commit

--- Hotel/obj/Debug/net8.0/Hotel.AssemblyInfo.cs
+++ Hotel/obj/Debug/net8.0/Hotel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb4669bd2581cd7cb77d4805c5446fd17a950d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
